efb38afff48b23a97f2e2eb0db48e691775ef58b
tdorado <tdorado@itba.edu.ar>
2020-08-31 13:43:34 -0300

    python does everything

--- visual/time_vs_particles.py
+++ visual/time_vs_particles.py
@@ -1,21 +1,21 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
 n_values = [2000, 3000, 5000]
 output_values = ["output2000", "output3000", "output5000"]
 
-for d in n_values:
-    cmd = 'java -jar target/ss-tp2-1.0.jar -n {:g} -o output{:g}'.format(d, d)
+for n in n_values:
+    cmd = 'java -jar ../target/ss-tp2-1.0.jar -n {:g} -o output{:g}'.format(n, n)
     print(cmd)
-    #os.system(cmd)
+    os.system(cmd)
 
 for file in output_values:
-    df = pd.read_csv('../out/' + file + '.csv')
-    df = df.sort_values(by='t')
+    df = pd.read_csv('out/' + file + '.csv')
     file = file.replace('output','')
     plt.plot(df['nR'], df['t'], label=file)
 
-plt.ylabel('Tiempo [mS]', fontsize=18)
-plt.xlabel('Fracción de partículas', fontsize=18)
+plt.ylabel('Tiempo [mS]', fontsize=16)
+plt.xlabel('Fracción de partículas recinto derecho', fontsize=16)
 plt.legend(title='Cantidad de partículas')
 plt.show()
