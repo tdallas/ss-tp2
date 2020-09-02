d654ff6123b17a1351121ef27e07482a4c4dafb3
tdorado <tdorado@itba.edu.ar>
2020-09-02 16:48:53 -0300

    all ready

--- visual/time_vs_particles.py
+++ visual/time_vs_particles.py
@@ -3,17 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 n_values = [2000, 3000, 5000]
-output_values = ["output2000", "output3000", "output5000"]
 
 for n in n_values:
     cmd = 'java -jar ../target/ss-tp2-1.0.jar -n {:g} -o output{:g}'.format(n, n)
     print(cmd)
     os.system(cmd)
 
-for file in output_values:
-    df = pd.read_csv('out/' + file + '.csv')
-    file = file.replace('output','')
-    plt.plot(df['nR'], df['t'], label=file)
+for n in n_values:
+    df = pd.read_csv('out/output{:g}.csv'.format(n))
+    plt.plot(df['nR'], df['t'], label=n)
 
 plt.ylabel('Tiempo [mS]', fontsize=16)
 plt.xlabel('Fracción de partículas recinto derecho', fontsize=16)
